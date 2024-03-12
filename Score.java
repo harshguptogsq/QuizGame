d1662ed10871ee2355b5ab923e8044e7915e5242
unknown <harsh.gupta080111@gmail.com>
2024-03-12 12:20:13 +0530

    quiz game

---
+++ Score.java
@@ -0,0 +1,39 @@
+
+package quizgame;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Score extends JFrame implements  ActionListener{
+    Score(String username , int score){
+        
+        setBounds(600, 150, 750, 550);
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("quizgame/icons/score.png"));
+        Image i2 = i1.getImage().getScaledInstance(300, 250, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel l1 = new JLabel(i3);
+        l1.setBounds(10, 200, 300, 250);
+        add(l1);
+        
+        JLabel l2 = new JLabel("Thank You " + username + " for Playing Simple Minds");
+         l2.setBounds(45, 30, 700, 30)       ;
+         l2.setFont(new Font("RALEWAY", Font.PLAIN, 26));
+         add(l2);
+         
+        JLabel l3 = new JLabel("Your Score is " + score);
+         l3.setBounds(350, 200, 300, 30);
+         l3.setFont(new Font("Jokerman", Font.PLAIN, 26));
+         l3.setForeground(new Color(199, 21, 133));
+         add(l3);
+    }
+    
+    public void actionPerformed(ActionEvent ae){
+        this.setVisible(false);
+      
+        new QuizGame().setVisible(true);
+    }
+}
